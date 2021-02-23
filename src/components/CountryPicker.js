6b887939ac459e9aa1e771dd5692257246f70e6c
MohammadMahdi <mamiriesfahani@gmail.com>
2021-02-23 13:43:27 +0330

    remove api file and logs

--- src/components/CountryPicker.js
+++ src/components/CountryPicker.js
@@ -4,15 +4,7 @@ export default function CountryPicker(props) {
 
     async function handle_select_option(e){
         props.setCountry(e.target.value)
-
         let res = props.countriesObj.filter(ele=>ele.Country === e.target.value)
-        // console.log(res) 
-        // console.log(res[0].Country)
-        // console.log(res[0].TotalConfirmed)
-        // console.log(res[0].TotalDeaths)
-        // console.log(res[0].TotalRecovered)
-        // console.log(res[0].Date)
-
         props.setDeaths(res[0].TotalDeaths)
         props.setConfirmed(res[0].TotalConfirmed)
         props.setRecovered(res[0].TotalRecovered)
